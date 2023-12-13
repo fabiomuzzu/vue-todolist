1c49b0d5f227ee8a03002a0570815c72804a5d05
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-12-13 15:39:25 +0100

    added todo_list in js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,28 @@ const { createApp } = Vue
 createApp({
     data() {
     return {
-
+        todo_list: [
+            {
+                text: 'Fare la spesa',
+                done: true,
+            },
+            {
+                text: 'Buttare la spazzatura',
+                done: false,
+            },
+            {
+                text: 'Fare benzina alla macchina',
+                done: true,
+            },
+            {
+                text: 'Ritirare i panni',
+                done: false,
+            },
+            {
+                text: 'Prendere l\'acqua',
+                done: true,
+            },
+        ]
     }
 },
 methods: {
